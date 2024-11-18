bf2d9e7932b18bc4a910ba7404653cda1b8d8a60
Slava Developer <slavikbezasvilij@gmail.com>
2024-11-18 19:39:00 +0500

    classwork 18-11-2024

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,23 @@
-'use strict'
+'use strict';
 
-const navOpenBtn = document.querySelector("[data-nav-open-btn")
-const navbar = document.querySelector("[data-navbar]")
-const navCloseBtn = document.querySelector("[data-nav-close-btn]")
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
 
-const navElems = [navOpenBtn, navCloseBtn]
+const navElems = [navOpenBtn, navCloseBtn];
 
-for (let i = 0; i < navElems.length; i++){
-    navElems[i].addEventListener("click", function() {
-        navbar.classList.toggle("active")
-    })
+for (let i = 0; i < navElems.length; i++) {
+    navElems[i].addEventListener("click", function () {
+        navbar.classList.toggle("active");
+    });
 }
 
 
 
-console.log(navElems);
-        
+
+const searchContainer = document.querySelector("[data-search-wrapper]");
+const searchBtn = document.querySelector("[data-search-btn]");
+
+searchBtn.addEventListener("click", function () {
+    searchContainer.classList.toggle("active");
+});
