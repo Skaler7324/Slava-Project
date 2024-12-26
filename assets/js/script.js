436bf3805e81945c1c1f2ea7dcd37471fa3c2148
FRONTEND <davlatsulton2016@gmail.com>
2024-12-26 17:05:36 +0500

    26-12-2024

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,35 +7,57 @@ const navCloseBtn = document.querySelector("[data-nav-close-btn]");
 const navElems = [navOpenBtn, navCloseBtn];
 
 for (let i = 0; i < navElems.length; i++) {
-    navElems[i].addEventListener("click", function () {
-        navbar.classList.toggle("active");
-    });
+  navElems[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+  });
 }
-
-
-
 
 const searchContainer = document.querySelector("[data-search-wrapper]");
 const searchBtn = document.querySelector("[data-search-btn]");
 
 searchBtn.addEventListener("click", function () {
-    searchContainer.classList.toggle("active");
+  searchContainer.classList.toggle("active");
 });
 
-
-const panelBtns = document.querySelectorAll('[data-panel-btn]');
-const sidePanels = document.querySelectorAll('[data-side-panel]');
+const panelBtns = document.querySelectorAll("[data-panel-btn]");
+const sidePanels = document.querySelectorAll("[data-side-panel]");
 
 for (let i = 0; i < panelBtns.length; i++) {
-    panelBtns[i].addEventListener("click", function() {
-        let clickedElemDataValue = this.dataset.panelBtn; 
-        
-        for (let i = 0; i < sidePanels.length; i++) {
-            if (clickedElemDataValue === sidePanels[i].dataset.sidePanel) {  
-                sidePanels[i].classList.toggle('active'); 
-            } else {
-                sidePanels[i].classList.remove('active'); 
-            }
-        }
-    });
+  panelBtns[i].addEventListener("click", function () {
+
+    let clickedElemDataValue = this.dataset.panelBtn;
+
+    for (let i = 0; i < sidePanels.length; i++) {
+
+      if (clickedElemDataValue === sidePanels[i].dataset.sidePanel) {
+        sidePanels[i].classList.toggle("active");
+      } else {
+        sidePanels[i].classList.remove("active");
+      }
+
+    }
+
+  });
+}
+
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 100 ? backTopBtn.classList.add("active")
+    : backTopBtn.classList.remove("active");
+});
+
+const productDisplay = document.querySelector("[data-product-display]");
+const productThumbnails = document.querySelectorAll("[data-product-thumbnail]");
+
+for (let i = 0; i < productThumbnails.length; i++) {
+  productThumbnails[i].addEventListener("click", function () {
+    productDisplay.src = this.src;
+    productDisplay.classList.add("fade-anim");
+
+    setTimeout(function () {
+      productDisplay.classList.remove("fade-anim");
+    }, 250);
+
+  });
 }
